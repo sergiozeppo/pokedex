ea3467f1a1fd3dc28aab2414a0bb6a0988920b62
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-01-29 22:23:55 +0500

    feat: update App component

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,12 +1,55 @@
 import { Component, ReactNode } from 'react';
 import Header from './views/Header/Header';
+import { fetchData } from './services/api';
 import './App.css';
+import Main from './views/Main/Main';
+import { Pokemon } from './types/types';
 
-class App extends Component {
+interface AppState {
+  pokemons: Pokemon[]; // Можно уточнить тип, если данные имеют определённую структуру
+  error: Error | null;
+  isFetching: boolean;
+}
+
+class App extends Component<AppState> {
+  state: AppState = {
+    pokemons: [],
+    error: null,
+    isFetching: false,
+  };
+  handleFetchData = async () => {
+    try {
+      const pokemons = await fetchData();
+      this.setState({
+        pokemons,
+        error: null,
+        isFetching: false,
+      });
+    } catch (error) {
+      this.setState({
+        pokemons: [],
+        error: error instanceof Error ? error : new Error('Unknown error'),
+        isFetching: false,
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.handleFetchData();
+  }
+
   render(): ReactNode {
+    const { pokemons, isFetching, error } = this.state;
     return (
       <div className="container">
         <Header />
+        {isFetching ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Error: {error.message}</p>
+        ) : (
+          <Main pokemons={pokemons} />
+        )}
       </div>
     );
   }
