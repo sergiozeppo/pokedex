a28137f4846d4c207902a1a1ea833ecb8786892f
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-02-07 15:08:43 +0500

    fix: fix usePagination hook issue

--- main/src/utils/usePagination/usePagination.tsx
+++ main/src/utils/usePagination/usePagination.tsx
@@ -1,22 +1,28 @@
-import { useState, useMemo, useCallback } from 'react';
+import { useState, useMemo, useCallback, useEffect } from 'react';
 import { Pokemon } from '../../types/types';
 
 export function usePagination(
-  allPokemons: Pokemon[],
+  data: Pokemon[],
   initialPage: number = 1,
   limit: number = 20
 ) {
   const [page, setPage] = useState(initialPage);
 
   const totalPages = useMemo(
-    () => Math.ceil(allPokemons.length / limit),
-    [allPokemons, limit]
+    () => Math.ceil(data.length / limit),
+    [data, limit]
   );
+
+  useEffect(() => {
+    if (page > totalPages) {
+      setPage(1);
+    }
+  }, [page, totalPages]);
 
   const currentData = useMemo(() => {
     const startIndex = (page - 1) * limit;
-    return allPokemons.slice(startIndex, startIndex + limit);
-  }, [allPokemons, page, limit]);
+    return data.slice(startIndex, startIndex + limit);
+  }, [data, page, limit]);
 
   const nextPage = useCallback(() => {
     setPage((prev) => Math.min(prev + 1, totalPages));
