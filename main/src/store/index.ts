bd37aca4bcb3aeed83977dfd263c6626413bc6fa
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-02-23 19:43:43 +0500

    feat: implement searchquery slice and hook

--- main/src/store/index.ts
+++ main/src/store/index.ts
@@ -1,8 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import selectedPokemonsSlice from './reducers/selectedPokemonsSlice';
+import { pokemonApi } from '../services/api';
+import allPokemonsSlice from './reducers/allPokemonsSlice';
+import currentPokemonsSlice from './reducers/currentPokemonsSlice';
+import loadingSlice from './reducers/loadingSlice';
+import searchQuerySlice from './reducers/searchQuerySlice';
+
+const rootReducer = combineReducers({
+  loading: loadingSlice,
+  selectedPokemonsSlice,
+  searchQuerySlice,
+  allPokemonsSlice,
+  currentPokemonsSlice,
+  [pokemonApi.reducerPath]: pokemonApi.reducer,
+});
 
 export const store = configureStore({
-  reducer: { selectedPokemonsSlice },
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
