59e3acec9958458d979555705ebd3ee3499cd10f
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-02-23 18:48:28 +0500

    fix: fix BrowserRouter

--- main/src/main.tsx
+++ main/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/index.ts';
 import { ThemeProvider } from './context/ThemeProvider.tsx';
