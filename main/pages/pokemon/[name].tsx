be42a8cdbc3ffe6f1b4397cadd087d5d93c2ef5f
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-03-03 23:24:35 +0500

    fix: fix CardDetails component

--- main/pages/pokemon/[name].tsx
+++ main/pages/pokemon/[name].tsx
@@ -1,8 +1,7 @@
-// import { useParams, useNavigate } from 'react-router-dom';
-// import './CardDetails.module.css';
-// import { PokemonColorPair } from '../../src/types/types';
+import styles from '../../src/views/CardDetails/CardDetails.module.css';
+import { PokemonColorPair } from '../../src/types/types';
 import PokeLoader from '../../src/components/PokeLoader/PokeLoader';
-// import { usePokemonBackground } from '../../src/utils/usePokemonBackground/usePokemonBackground';
+import { usePokemonBackground } from '../../src/utils/usePokemonBackground/usePokemonBackground';
 import { useGetPokemonOutletDetailsQuery } from '../../src/services/api';
 import { useRouter } from 'next/router';
 import RootLayout from '../RootLayout';
@@ -11,163 +10,167 @@ import PokemonLayout from './PokemonLayout';
 const CardDetails = () => {
   const router = useRouter();
   const name = router.query.name as string;
-  console.log(name);
 
   const {
     data: pokemonData,
     isLoading,
     isError,
     error,
-  } = useGetPokemonOutletDetailsQuery('');
-  // const navigate = useNavigate();
+  } = useGetPokemonOutletDetailsQuery(name || '');
 
-  // const handleClick = () => navigate('/');
-  // const colorsArray = usePokemonBackground(
-  //   pokemonData?.types || []
-  // ) as PokemonColorPair[];
+  const handleClick = () => router.push('/');
 
-  if (!name) return <div className="card-details">No Pokémon selected</div>;
+  const colorsArray = usePokemonBackground(
+    pokemonData?.types || []
+  ) as PokemonColorPair[];
 
-  const id =
-    // `#${pokemonData?.id.toString().padStart(3, '0')}`
-    '#000';
+  if (!name)
+    return <div className={styles['card-details']}>No Pokémon selected</div>;
+
+  const id = `#${pokemonData?.id?.toString().padStart(3, '0')}`;
   const img = pokemonData?.imageUrl || '/assets/img/poke-loader.png';
+
+  console.log(router.query);
 
   return (
     <RootLayout>
       <PokemonLayout>
         <div
-          className="card-details"
+          className={styles['card-details']}
           style={{
-            background:
-              // colorsArray[0]?.primary ||
-              '#ccc',
+            background: colorsArray[0]?.primary || '#ccc',
           }}
         >
           {isLoading ? (
             <PokeLoader data-testid="poke-loader" />
           ) : isError ? (
-            <div className="error">
+            <div className={styles['error']}>
               {(error as Error)?.message || 'Failed to load Pokémon details'}
             </div>
           ) : pokemonData ? (
             <>
-              <div className="details-container">
-                <div className="details-header">
-                  <div className="details-title">
-                    <div className="details-close"></div>
-                    <span className="details-h2">{pokemonData.name}</span>
-                    <span className="details-id">{id}</span>
+              <div className={styles['details-container']}>
+                <div className={styles['details-header']}>
+                  <div className={styles['details-title']}>
+                    <div
+                      className={styles['details-close']}
+                      onClick={handleClick}
+                    ></div>
+                    <span className={styles['details-h2']}>
+                      {pokemonData.name}
+                    </span>
+                    <span className={styles['details-id']}>{id}</span>
                   </div>
 
-                  <div className="details-img">
+                  <div className={styles['details-img']}>
                     <img
                       src={img}
                       alt={pokemonData.name}
-                      className="detail-image"
+                      className={styles['detail-image']}
                     />
                   </div>
                 </div>
 
-                <div className="details-card">
-                  <div className="details-types">
-                    {/* {pokemonData.types.map((type, index) => (
-                  <div
-                    key={index}
-                    className="details-type"
-                    style={{ background: colorsArray[index].primary }}
-                  >
-                    <span
-                      className="details-type-name"
-                      data-testid="pokemon-type"
-                    >
-                      {type.type.name}
-                    </span>
-                  </div>
-                ))} */}
+                <div className={styles['details-card']}>
+                  <div className={styles['details-types']}>
+                    {pokemonData.types.map((type, index) => (
+                      <div
+                        key={index}
+                        className={styles['details-type']}
+                        style={{ background: colorsArray[index].primary }}
+                      >
+                        <span
+                          className={styles['details-type-name']}
+                          data-testid="pokemon-type"
+                        >
+                          {type.type.name}
+                        </span>
+                      </div>
+                    ))}
                   </div>
 
                   <div
-                    className="details-about"
+                    className={styles['details-about']}
                     style={{
-                      color:
-                        // colorsArray[0].primary
-                        'red',
+                      color: colorsArray[0].primary,
                     }}
                   >
                     About
                   </div>
 
-                  <div className="details-attributes">
-                    <div className="details-attr-section">
-                      <div className="details-attr">
+                  <div className={styles['details-attributes']}>
+                    <div className={styles['details-attr-section']}>
+                      <div className={styles['details-attr']}>
                         <img
-                          className="details-img-weight"
+                          className={styles['details-img-weight']}
                           src="/assets/img/weight.svg"
                           alt="weight"
                         />
-                        <span className="details-attr-data">
+                        <span className={styles['details-attr-data']}>
                           {pokemonData.weight / 10} kg
                         </span>
                       </div>
-                      <span className="details-attr-text">Weight</span>
+                      <span className={styles['details-attr-text']}>
+                        Weight
+                      </span>
                     </div>
 
-                    <span className="divider" />
+                    <span className={styles['divider']} />
 
-                    <div className="details-attr-section">
-                      <div className="details-attr">
+                    <div className={styles['details-attr-section']}>
+                      <div className={styles['details-attr']}>
                         <img
-                          className="details-img-height"
+                          className={styles['details-img-height']}
                           src="/assets/img/straighten.svg"
                           alt="height"
                         />
-                        <span className="details-attr-data">
+                        <span className={styles['details-attr-data']}>
                           {pokemonData.height / 10} m
                         </span>
                       </div>
-                      <span className="details-attr-text">Height</span>
+                      <span className={styles['details-attr-text']}>
+                        Height
+                      </span>
                     </div>
 
-                    <span className="divider" />
+                    <span className={styles['divider']} />
 
-                    <div className="details-attr-section">
-                      <div className="details-attr-moves">
-                        {/* {pokemonData.moves.slice(0, 2).map((move, index) => (
-                      <span key={index} className="details-attr-moves-text">
-                        {move.move.name}
-                      </span>
-                    ))} */}
+                    <div className={styles['details-attr-section']}>
+                      <div className={styles['details-attr-moves']}>
+                        {pokemonData.moves.slice(0, 2).map((move, index) => (
+                          <span
+                            key={index}
+                            className={styles['details-attr-moves-text']}
+                          >
+                            {move.move.name}
+                          </span>
+                        ))}
                       </div>
-                      <span className="details-attr-text">Moves</span>
+                      <span className={styles['details-attr-text']}>Moves</span>
                     </div>
                   </div>
 
                   <div
-                    className="details-about"
+                    className={styles['details-about']}
                     style={{
-                      color:
-                        // colorsArray[0].primary
-                        'red',
+                      color: colorsArray[0].primary,
                     }}
                   >
                     Base Stats
                   </div>
 
-                  <div className="details-stats">
+                  <div className={styles['details-stats']}>
                     <div
-                      className="details-stats-label-section"
+                      className={styles['details-stats-label-section']}
                       style={{
-                        color:
-                          // colorsArray[0]?.primary ||
-                          '#ccc',
+                        color: colorsArray[0]?.primary || '#ccc',
                       }}
                     >
                       {['HP', 'ATK', 'DEF', 'SATK', 'SDEF', 'SPD'].map(
                         (label) => (
                           <span
                             key={label}
-                            className="details-stats-label-name"
+                            className={styles['details-stats-label-name']}
                           >
                             {label}
                           </span>
@@ -175,32 +178,36 @@ const CardDetails = () => {
                       )}
                     </div>
 
-                    <div className="stats-divider"></div>
+                    <div className={styles['stats-divider']}></div>
 
-                    <div className="data-list">
-                      {/* {pokemonData.stats.map((stat, index) => (
-                    <div key={index} className="stat-item">
-                      <div className="stat-value">{stat.base_stat}</div>
-                      <div
-                        style={{
-                          width: '100%',
-                          height: '4px',
-                          backgroundColor: colorsArray[0]?.secondary || '#eee',
-                          borderRadius: '4px',
-                          overflow: 'hidden',
-                        }}
-                      >
-                        <div
-                          style={{
-                            width: `${(stat.base_stat / 250) * 100}%`,
-                            height: '100%',
-                            backgroundColor: colorsArray[0]?.primary || '#ccc',
-                            transition: 'width 0.5s ease',
-                          }}
-                        ></div>
-                      </div>
-                    </div>
-                  ))} */}
+                    <div className={styles['data-list']}>
+                      {pokemonData.stats.map((stat, index) => (
+                        <div key={index} className={styles['stat-item']}>
+                          <div className={styles['stat-value']}>
+                            {stat.base_stat}
+                          </div>
+                          <div
+                            style={{
+                              width: '100%',
+                              height: '4px',
+                              backgroundColor:
+                                colorsArray[0]?.secondary || '#eee',
+                              borderRadius: '4px',
+                              overflow: 'hidden',
+                            }}
+                          >
+                            <div
+                              style={{
+                                width: `${(stat.base_stat / 250) * 100}%`,
+                                height: '100%',
+                                backgroundColor:
+                                  colorsArray[0]?.primary || '#ccc',
+                                transition: 'width 0.5s ease',
+                              }}
+                            ></div>
+                          </div>
+                        </div>
+                      ))}
                     </div>
                   </div>
                 </div>
