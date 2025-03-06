f29b0ffa28b863bcc813a2d7b000e315e5d53ded
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-03-06 20:57:31 +0500

    fix: fix searchBar not URL updating issue

--- main/src/components/SearchBar/SearchBar.tsx
+++ main/src/components/SearchBar/SearchBar.tsx
@@ -7,9 +7,11 @@ import { useDispatch } from 'react-redux';
 import { useSearchQuery } from '../../utils/useSearchQuery/useSearchQuery';
 import { setSearchQuery } from '../../store/reducers/searchQuerySlice';
 import styles from './SearchBar.module.css';
+import { useRouter } from 'next/navigation';
 
 function SearchBar() {
   const dispatch = useDispatch();
+  const router = useRouter();
   const [hasError, setHasError] = useState(false);
   const [inputValue, setInputValue] = useState('');
   const { updateSearchQuery } = useSearchQuery();
@@ -21,12 +23,14 @@ function SearchBar() {
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       dispatch(setSearchQuery(inputValue.trim()));
+      router.replace(`/?q=${inputValue.trim()}`);
       updateSearchQuery(inputValue.trim());
     }
   };
 
   const handleSearchClick = () => {
     dispatch(setSearchQuery(inputValue.trim()));
+    router.replace(`/?q=${inputValue.trim()}`);
     updateSearchQuery(inputValue.trim());
   };
 
