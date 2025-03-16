c2b2e3850e9c10028928b8f4fb893f385ae54e3b
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-03-16 14:55:04 +0500

    feat: add validation schema and validation rules

---
+++ forms/src/constants/validation-schema.ts
@@ -0,0 +1,61 @@
+import { z } from 'zod';
+import {
+  AGE_MAX,
+  EMAIL,
+  FIRST_UPPERCASE_LETTER,
+  IMAGE_MAX_SIZE,
+  LOWERCASE_LETTER,
+  NUMBER,
+  PICTURE_FILE_TYPES,
+  SPECIAL_CHARACTER,
+  UPPERCASE_LETTER,
+} from './rules';
+import { COUNTRIES_LIST } from './constants';
+
+export const validationSchema = z
+  .object({
+    name: z
+      .string()
+      .regex(
+        FIRST_UPPERCASE_LETTER,
+        'Name must start with an uppercase letter'
+      ),
+    age: z
+      .number()
+      .positive('Age must be > 0')
+      .refine((age) => age <= AGE_MAX, 'Age must be truthful'),
+    email: z.string().refine((value) => EMAIL.test(value), {
+      message: 'Invalid email format. Example: example@domain.com',
+    }),
+    password: z
+      .string()
+      .regex(NUMBER, 'Must include a number')
+      .regex(UPPERCASE_LETTER, 'Must include an uppercase letter')
+      .regex(LOWERCASE_LETTER, 'Must include a lowercase letter')
+      .regex(SPECIAL_CHARACTER, 'Must include a special character'),
+    confirmPassword: z.string(),
+    gender: z.enum(['male', 'female', 'other']),
+    picture: z
+      .custom<File>((file) => file instanceof File, 'Invalid file input')
+      .refine((file) => file && PICTURE_FILE_TYPES.includes(file.type), {
+        message: 'Only PNG and JPEG are allowed',
+      })
+      .refine((file) => file && file.size <= IMAGE_MAX_SIZE, {
+        message: 'Filesize should be less than 2MB',
+      }),
+    country: z
+      .string()
+      .nonempty('Country is required')
+      .refine((value) => COUNTRIES_LIST.includes(value), {
+        message: 'Should be a country from the list',
+      }),
+    terms: z
+      .boolean()
+      .refine((val) => val === true, 'You must accept the terms'),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords must match',
+    path: ['confirmPassword'],
+  });
+
+export type inferred = z.infer<typeof validationSchema>;
